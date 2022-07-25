a2937fdef9105bd1d4a900580c5fcf62bd3f1ab7
edmarr2 <edmar_espinola@hotmail.com>
2022-07-25 15:59:07 -0400

    refactor: make SignUpController conforms to a protocol

--- scr/presentation/controllers/signup.spec.ts
+++ scr/presentation/controllers/signup.spec.ts
@@ -28,4 +28,17 @@ test('Should return 400 if no email is provided', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
 })
+test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'edmar',
+        email: 'edmar.espinola@mail.com',
+        password_confirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password_confirmation'))
+})
 })
--- scr/presentation/controllers/signup.ts
+++ scr/presentation/controllers/signup.ts
@@ -1,9 +1,10 @@
 import { MissingParamError } from '../errors/missing-param-erro'
 import { badRequest } from '../helper/http-helper'
+import { Controller } from '../protocols/controller'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: any): any {
-    const requiredFields = ['email', 'password', 'name']
+    const requiredFields = ['email', 'password', 'name', 'password_confirmation']
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
