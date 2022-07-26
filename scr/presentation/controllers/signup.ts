7d7bd9f8c83b0600b7d9a339a47d190608175e5f
edmarr2 <edmar_espinola@hotmail.com>
2022-07-26 14:51:48 -0400

    test: ensure SignUpController call EmailsValidator with correct email

--- scr/presentation/controllers/signup.ts
+++ scr/presentation/controllers/signup.ts
@@ -1,8 +1,15 @@
 import { MissingParamError } from '../errors/missing-param-erro'
 import { badRequest } from '../helper/http-helper'
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: any): any {
     const requiredFields = ['email', 'password', 'name', 'password_confirmation']
 
@@ -10,6 +17,10 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
